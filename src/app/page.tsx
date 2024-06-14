c48c88cda7e2a55b63cfabb6244e2a7e3105996e
Manita Paudel <paudel04manita@gmail.com>
2024-06-14 22:22:51 +0545

    Update the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProfileContainer from "@/app/components/profile-container";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <ProfileContainer />
     </main>
   );
